00ab7ca4d192a240bbbc8f56fd5c3b3b22cd2098
Adi Andrea <dekz.itfz@gmail.com>
2017-08-24 10:21:12 +0800

    init HelloWorldTest class

---
+++ app/src/androidTest/java/id/dekz/retrofitexample/HelloWorldTest.java
@@ -0,0 +1,14 @@
+package id.dekz.retrofitexample;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.runner.RunWith;
+
+/**
+ * Created by DEKZ on 8/24/2017.
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class HelloWorldTest {
+
+}
