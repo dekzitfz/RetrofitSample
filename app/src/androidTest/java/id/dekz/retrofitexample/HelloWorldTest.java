58679a8b267c6b338f28f823cf878ade5890ffca
Adi Andrea <dekz.itfz@gmail.com>
2017-08-24 10:33:09 +0800

    create test validateHelloWorld

--- app/src/androidTest/java/id/dekz/retrofitexample/HelloWorldTest.java
+++ app/src/androidTest/java/id/dekz/retrofitexample/HelloWorldTest.java
@@ -4,7 +4,13 @@ import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 /**
  * Created by DEKZ on 8/24/2017.
@@ -17,6 +23,9 @@ public class HelloWorldTest {
     public ActivityTestRule<MainActivity> rule =
             new ActivityTestRule<>(MainActivity.class);
 
-
+    @Test
+    public void validateHelloWorld(){
+        onView(withId(R.id.tvHello)).check(matches(withText("Hello World!")));
+    }
 
 }
